16dd407811607b0cccd6dfa3b5d5d4e8d82dc8c7
Yu Hashimoto <aoiasagao.g@gmail.com>
2018-01-23 14:53:20 +0900

    初期

---
+++ entry.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>課題・登録フォーム　テスト</title>
+    <link rel="stylesheet" href="css/sanitize.css"> 
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>登録フォーム</h1>
+    <p>Webサイトの内容は<a href="./" target="_blank">こちらから</a>確認できます。</p>
+    <form action="insert.php" method="post">
+        <ul>
+            <li class="form-item">
+                <label for="title">書籍名</label>
+                <input type="text" name="title" id="title" class="uk-input">
+            </li>
+            <li class="form-item">
+                <label for="url">URL</label>
+                <input type="url" name="url" id="url" class="uk-input">
+            </li>
+            <li class="form-item">
+                <label for="detail">コメント</label>
+                <textarea name="text" id="text" cols="30" rows="10" class="uk-textarea"></textarea>
+            </li>
+        </ul>
+        <input type="submit" value="送信">
+    </form>    
+</div>
+</body>
+</html>
